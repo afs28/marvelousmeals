8008d8234ecd7ff87846ffc5e19fab8e013e05d0
birgitta <birgittaye@gmail,com>
2022-08-31 16:28:22 +0000

    bæta nafninu mínu (Birgitta) við skrána samhvamt leiðbeiningum

--- src/main/java/is/hi/hbv501g/hugbo/HugboApplication.java
+++ src/main/java/is/hi/hbv501g/hugbo/HugboApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+    // this is team member Birgitta Ýr Eyþórsdóttir (bye1@hi.is)
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class HugboApplication {
 
