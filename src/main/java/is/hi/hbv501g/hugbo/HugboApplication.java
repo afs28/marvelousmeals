15a947f51f3bd20992e352f1e490c9a7d486f33c
applemuffinhead <hkv5@hi.is>
2022-09-01 09:39:25 +0000

    Fyrsta commit / bara komment

--- src/main/java/is/hi/hbv501g/hugbo/HugboApplication.java
+++ src/main/java/is/hi/hbv501g/hugbo/HugboApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
     // this is team member Birgitta Ýr Eyþórsdóttir (bye1@hi.is)
     // this is team member Heba Sólveig Heimisdóttir (hsh58@hi.is)
+    // this is team member Hrefna Karen Valgarðsdóttir (hkv5@hi.is)
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class HugboApplication {
 
