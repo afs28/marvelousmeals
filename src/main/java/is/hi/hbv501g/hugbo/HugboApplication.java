7fce5ca3fd4b346e0f5a59b739e63f0b2c3e38f6
hebasolveig <hsh58@hi.is>
2022-09-01 09:38:02 +0000

    heba is here

--- src/main/java/is/hi/hbv501g/hugbo/HugboApplication.java
+++ src/main/java/is/hi/hbv501g/hugbo/HugboApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
     // this is team member Birgitta Ýr Eyþórsdóttir (bye1@hi.is)
+    // this is team member Heba Sólveig Heimisdóttir (hsh58@hi.is)
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class HugboApplication {
 
