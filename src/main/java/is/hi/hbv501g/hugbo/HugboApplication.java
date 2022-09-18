a3d6159eeed5c7f3e156b5b7bd1d9edc5e24fb35
arnar <afs28@hi.is>
2022-09-18 17:49:11 +0000

    Second commit, added some functionality

--- src/main/java/is/hi/hbv501g/hugbo/HugboApplication.java
+++ src/main/java/is/hi/hbv501g/hugbo/HugboApplication.java
@@ -2,12 +2,12 @@ package is.hi.hbv501g.hugbo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 
     // this is team member Birgitta Ýr Eyþórsdóttir (bye1@hi.is)
     // this is team member Heba Sólveig Heimisdóttir (hsh58@hi.is)
     // this is team member Hrefna Karen Valgarðsdóttir (hkv5@hi.is)
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class HugboApplication {
 
     public static void main(String[] args) {
