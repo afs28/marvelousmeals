86aa6bac7e6ad1908219c6c356233d104c562b78
birgitta <birgittaye@gmail,com>
2022-10-15 15:09:55 +0000

    færði hluti frá recipeController í userController, setti upp föl í User Repo án implimentation

--- src/main/java/is/hi/hbv501g/hugbo/Persistence/Repositories/UserRepository.java
+++ src/main/java/is/hi/hbv501g/hugbo/Persistence/Repositories/UserRepository.java
@@ -1,6 +1,8 @@
 package is.hi.hbv501g.hugbo.Persistence.Repositories;
 import is.hi.hbv501g.hugbo.Persistence.Entities.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * The recipe user repository.
@@ -11,5 +13,10 @@ public interface UserRepository extends CrudRepository<User, Integer> {
 /*
                                             TO BE RECONFIGURED
  */
+    Iterable<User> findAll();
     User findUserByUserId(Integer UserId);
+
+    String addUser(String FirstName, String LastName);
+
+
 }
