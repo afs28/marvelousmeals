3acdd6d7948410e19d9d09aaf2ffc4368ec1d039
arnar <afs28@hi.is>
2022-10-19 20:14:31 +0000

    Crashes begone

--- src/main/java/is/hi/hbv501g/hugbo/Controllers/LoginController.java
+++ src/main/java/is/hi/hbv501g/hugbo/Controllers/LoginController.java
@@ -2,7 +2,6 @@ package is.hi.hbv501g.hugbo.Controllers;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class LoginController {
--- src/main/java/is/hi/hbv501g/hugbo/Controllers/RecipeUserController.java
+++ src/main/java/is/hi/hbv501g/hugbo/Controllers/RecipeUserController.java
@@ -1,15 +1,8 @@
 package is.hi.hbv501g.hugbo.Controllers;
 
-import is.hi.hbv501g.hugbo.Persistence.Entities.RecipeUser;
-import is.hi.hbv501g.hugbo.Services.RecipeUserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+
 
 @Controller
 public class RecipeUserController {
