d8d608fcb8ee0f761e8ba78ed0b848c08af54cca
applemuffinhead <hkv5@hi.is>
2022-10-23 16:33:16 +0000

    Pullaði áðan en það kom allt nema RecipeUserRepository. Setti það inn manually. Sjá hvað gerist ef ég committa þetta svona.

---
+++ src/main/java/is/hi/hbv501g/hugbo/Persistence/Repositories/RecipeUserRepository.java
@@ -0,0 +1,20 @@
+package is.hi.hbv501g.hugbo.Persistence.Repositories;
+import is.hi.hbv501g.hugbo.Persistence.Entities.RecipeUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import java.util.List;
+
+/**
+ * The recipe user repository.
+ * @Authors: AFS
+ *
+ */
+
+public interface RecipeUserRepository extends JpaRepository<RecipeUser, Long> {
+
+    RecipeUser save(RecipeUser recipeUser);
+    void delete(RecipeUser recipeUser);
+    List<RecipeUser> findAll();
+    RecipeUser findByRecipeUsername(String recipeUsername);
+}
